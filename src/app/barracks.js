d01bb7fc062b326a7c27fcfcb3e68fd7fa0f9fa4
Michael Wolfenden <michael.wolfenden@gmail.com>
2016-07-08 16:32:59 +1000

    SAVEPOINT

--- src/app/barracks.js
+++ src/app/barracks.js
@@ -1,4 +1,35 @@
-const barracks = () => {}
+const wrapOnError = (onError) => (err) => {
+    if (err) onError(err)
+}
+
+const defaultOnError = (err) => {
+    throw err
+}
+
+const barracks = (handlers = {}) => {
+    const onError = wrapOnError(handlers.onError || defaultOnError)
+    const onAction = handlers.onAction
+    const onStateChange = handlers.onStateChange
+
+    let state$ = {}
+
+    const models$ = []
+
+    const setModel = (model) => models$.push(model)
+
+    const getState = (options = {}) => {
+        if (options.freeze === false) {
+            return { ...state$ }
+        }
+
+        return Object.freeze({ ...state$ })
+    }
+
+    return {
+        model: setModel,
+        state: getState,
+    }
+}
 
 module.exports = barracks
 
--- src/app/barracks.specs.js
+++ src/app/barracks.specs.js
@@ -6,5 +6,17 @@ describe('barracks', () => {
     it('should be a function', () => {
         assert(typeof barracks === 'function')
     })
+
+    describe('should return an object', () => {
+        const store = barracks()
+
+        it('containing a model function', () => {
+            assert(typeof store.model === 'function')
+        })
+
+        it('containing a state function', () => {
+            assert(typeof store.state === 'function')
+        })
+    })
 })
 
